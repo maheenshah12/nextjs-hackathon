973fbd1cc954b6f0e92a117985d73e78f9368eae
maheenshah12 <anabiashah090@gmail.com>
2024-12-19 01:23:33 +0500

    second upload done

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        // title: ["Roboto" ,"sans-serif"],
+        title:["Inter","sans-serif"], 
+        font: ["poppins", "sans-serif"]
+      }
+
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
+    },
+  },
+  plugins: [],
+} satisfies Config;
