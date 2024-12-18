36052be30a8c22e910041af99f8a6812341004bf
maheenshah12 <anabiashah090@gmail.com>
2024-12-19 01:25:41 +0500

    third upload done

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,27 @@
+// import Image from "next/image";
+// import logo from "@/app/assets/logo.png"
+// import frame from "@/app/assets/Frame.png"
+// import frame1 from "@/app/assets/Frame1.png"
+// import para1 from "@/app/assets/para1.png" 
+// import { IoCartOutline } from "react-icons/io5";
+// import { IoMdContact } from "react-icons/io";
+
+export default function Navbar(){
+    return(
+        <div></div>
+    //     <div className="absolute w-[1240px] h-[48px] top-[62px] left-[100px] flex flex-row  justify-around items-center">
+    //     <Image src={logo} alt="logo" className="w-[160px] h-[22px] leading-[32px]"/>
+
+    //     <Image src={frame} alt="frame" className="flex flex-row w-[321px] h-[22px] gap-[24px]"/>
+    //    <div className="w-[577px] h-[48px] bg-[#F0F0F0] rounded-[62px] py-[12px] px-[16px] gap-[12px] flex">
+    //   <Image src={frame1} alt="frame" className="w-[24px] h-[24px]"/>
+    //   <Image src={para1} alt="para" className="w-[151px] h-[22px] font-title"/>
+    //    </div>
+    //    <div className="w-[100px] h-[24px] gap-[14px] flex">
+    //    <IoCartOutline className="w-6 h-6"/>
+    //    <IoMdContact className="w-6 h-6"/>
+    //    </div>
+    //   </div>
+
+    )
+}
